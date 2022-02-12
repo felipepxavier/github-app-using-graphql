0fe342406caf47421dc02ef0ae72847437f86a7d
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2022-02-12 15:10:15 -0300

    test: should render the title

---
+++ packages/app/src/components/Card/.test.tsx
@@ -0,0 +1,14 @@
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from '../../utils/tests/helpers';
+import { Card } from '.';
+
+describe('<Card />', () => {
+  it('should render the title', () => {
+    const cardProps = {
+      title: 'Pesquise um username',
+    };
+    renderWithTheme(<Card {...cardProps} />);
+
+    expect(screen.getByRole('heading', { name: /`pesquise um username`/i }));
+  });
+});
