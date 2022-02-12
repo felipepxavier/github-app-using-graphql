15f7915c57d75d0b41cd22a96523f07bd6f2bf3f
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2022-02-12 14:40:12 -0300

    chore: add storybook/config

---
+++ packages/app/.storybook/preview.js
@@ -0,0 +1,22 @@
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from '../src/styles/global'
+import theme from '../src/styles/theme'
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Story />
+    </ThemeProvider>
+  )
+]
