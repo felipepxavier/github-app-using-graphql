f5000763fbc989e7464724505802ef36deee059d
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2022-02-11 21:16:48 -0300

    chore: add ignore files

--- packages/graphql-api/jest.config.js
+++ packages/graphql-api/jest.config.js
@@ -2,6 +2,7 @@ const options = {
   testEnvironment: 'node',
   verbose: true,
   collectCoverageFrom: ['**/*.js'],
+  coveragePathIgnorePatterns: ['coverage/*', 'jest.config.js'],
 };
 
 module.exports = options;
