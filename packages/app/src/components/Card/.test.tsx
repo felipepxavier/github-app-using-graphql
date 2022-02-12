60583716ff83a27dfc96cbf5741e9ffa659a72ba
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2022-02-12 15:44:40 -0300

    test: should render children correctly

--- packages/app/src/components/Card/.test.tsx
+++ packages/app/src/components/Card/.test.tsx
@@ -11,4 +11,16 @@ describe('<Card />', () => {
 
     expect(screen.getByRole('heading', { name: /pesquise um username/i }));
   });
+
+  
+  it('should render children correctly', () => {
+
+    const cardProps = {
+      title: 'Pesquise um username',
+    };
+    renderWithTheme(<Card {...cardProps}><span>children</span></Card>);
+
+    expect(screen.getByText('children')).toBeInTheDocument();
+  })
+  
 });
