24c7cfd8e72f542b447f6298e9524508e3fff606
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2022-02-12 13:19:13 -0300

    feat: global styles and theme

---
+++ packages/app/src/styles/theme.ts
@@ -0,0 +1,32 @@
+export default {
+  border: {
+    radius: '0.4rem',
+  },
+  font: {
+    family:
+      "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    light: 300,
+    normal: 400,
+    bold: 600,
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '1.8rem',
+      xlarge: '2.0rem',
+      xxlarge: '2.8rem',
+    },
+  },
+  colors: {
+    primary: '#4562ba',
+    secondary: '#eee',
+    grayLight: '#ddd',
+  },
+  spacings: {
+    xxsmall: '0.4rem',
+    xsmall: '0.8rem',
+    small: '1.6rem',
+    medium: '2.4rem',
+    large: '3.2rem',
+  },
+};
