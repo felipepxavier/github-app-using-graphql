f2d2cfbdfd79488f8abb310cf0a2f305dc8f9589
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2022-02-12 16:37:58 -0300

    refactor: create makeSut

--- packages/app/src/components/Card/Card.test.tsx
+++ packages/app/src/components/Card/Card.test.tsx
@@ -2,29 +2,22 @@ import { screen } from '@testing-library/react';
 import { renderWithTheme } from '../../utils/tests/helpers';
 import { Card } from '.';
 
+const makeSut = (
+  title = 'Pesquise um username',
+  children = <span>children</span>
+) => {
+  renderWithTheme(<Card title={title}>{children}</Card>);
+};
+
 describe('<Card />', () => {
   it('should render the title', () => {
-    const cardProps = {
-      title: 'Pesquise um username',
-    };
-    renderWithTheme(
-      <Card {...cardProps}>
-        <span>children</span>
-      </Card>
-    );
+    makeSut();
 
     expect(screen.getByRole('heading', { name: /pesquise um username/i }));
   });
 
   it('should render children correctly', () => {
-    const cardProps = {
-      title: 'Pesquise um username',
-    };
-    renderWithTheme(
-      <Card {...cardProps}>
-        <span>children</span>
-      </Card>
-    );
+    makeSut();
 
     expect(screen.getByText('children')).toBeInTheDocument();
   });
