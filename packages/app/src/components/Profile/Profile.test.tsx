cea47e80d3b0406bfa00b339a150cc98dc2aab1c
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2022-02-13 16:11:38 -0300

    test: should render redirect button to repositories of user

--- packages/app/src/components/Profile/Profile.test.tsx
+++ packages/app/src/components/Profile/Profile.test.tsx
@@ -24,4 +24,25 @@ describe('<Profile />', () => {
     expect(screen.getByText(myInfo.email)).toBeInTheDocument();
     expect(screen.getByText(myInfo.location)).toBeInTheDocument();
   });
+
+  it('should render redirect button to repositories of user', () => {
+    const myInfo = {
+      avatar_url: 'https://avatars.githubusercontent.com/u/43517062?v=4',
+      email: 'felipe@gmail.com',
+      location: 'Denver, CO',
+      name: 'Felipe Pedroso Xavier',
+      username: 'felipepxavier',
+      public_repos: 32,
+      bio: 'Loren ipsun',
+      followers: 15,
+      following: 10,
+    };
+    renderWithTheme(<Profile userData={myInfo} />);
+
+    const buttonRepositories = screen.getByRole('button', {
+      name: /repositorios/i,
+    });
+
+    expect(buttonRepositories).toBeInTheDocument();
+  });
 });
