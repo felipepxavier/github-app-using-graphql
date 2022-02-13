d18efb5048198df0c72c85cff0a8c6ac6bf418d6
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2022-02-13 10:37:48 -0300

    feat: add provider styled-components

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,5 +1,15 @@
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
+import GlobalStyles from './styles/global';
+import Main from 'pages/Main';
+
 function App() {
-  return <div>app</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Main />
+    </ThemeProvider>
+  );
 }
 
 export default App;
