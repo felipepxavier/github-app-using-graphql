06eff6e400c57c6f8d8ad077813357ea6c23ed1a
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2022-02-13 11:30:03 -0300

    test: should render icon of title if existing

--- packages/app/src/components/Card/Card.test.tsx
+++ packages/app/src/components/Card/Card.test.tsx
@@ -1,12 +1,28 @@
 import { screen } from '@testing-library/react';
 import { renderWithTheme } from '../../utils/tests/helpers';
+import { FaGithub } from 'react-icons/fa';
 import { Card } from '.';
 
+type makeSutProps = {
+  title?: string;
+  children?: JSX.Element;
+  iconTitle?: JSX.Element;
+};
 const makeSut = (
-  title = 'Pesquise um username',
-  children = <span>children</span>
+  {
+    title = 'Pesquise um username',
+    children = <span>children</span>,
+    iconTitle,
+  }: makeSutProps = {
+    title: 'Pesquise um username',
+    children: <span>children</span>,
+  }
 ) => {
-  renderWithTheme(<Card title={title}>{children}</Card>);
+  renderWithTheme(
+    <Card title={title} iconTitle={iconTitle}>
+      {children}
+    </Card>
+  );
 };
 
 describe('<Card />', () => {
@@ -20,5 +36,13 @@ describe('<Card />', () => {
     makeSut();
 
     expect(screen.getByText('children')).toBeInTheDocument();
+  });
+
+  it('should render icon of title if existing', () => {
+    makeSut({
+      iconTitle: <FaGithub data-testid="icon" />,
+    });
+
+    expect(screen.getByTestId('icon')).toBeInTheDocument();
   });
 });
