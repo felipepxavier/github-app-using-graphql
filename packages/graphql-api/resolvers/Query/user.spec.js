13bd34bd4f3a525b808705e1772fb490b0da22ad
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2022-02-13 21:07:08 -0300

    test: should render default value if not exists value

--- packages/graphql-api/resolvers/Query/user.spec.js
+++ packages/graphql-api/resolvers/Query/user.spec.js
@@ -1,11 +1,24 @@
+const api = require('../../services/api');
 const { user } = require('./user');
+
+jest.mock('../../services/api', () => ({
+  get: jest.fn(),
+}));
 
 describe('user()', () => {
   it('should call correctly', async () => {
     const myInfo = {
+      id: 10,
       name: 'Felipe Pedroso Xavier',
       location: 'Gravataí - RS',
+      description: 'não informado',
+      svn_url: 'myUrl',
+      visibility: 'public',
+      language: 'Javascript',
     };
+    api.get.mockResolvedValue({
+      data: myInfo,
+    });
 
     const usrData = await user(
       {},
@@ -16,5 +29,31 @@ describe('user()', () => {
 
     expect(usrData.name).toEqual(myInfo.name);
     expect(usrData.location).toEqual(myInfo.location);
+  });
+
+  it('should render default value if not exists value', async () => {
+    const myInfo = {
+      id: 10,
+      name: 'Felipe Pedroso Xavier',
+      location: null,
+      email: null,
+      description: 'não informado',
+      svn_url: 'myUrl',
+      visibility: 'public',
+      language: 'Javascript',
+    };
+    api.get.mockResolvedValue({
+      data: myInfo,
+    });
+
+    const usrData = await user(
+      {},
+      {
+        input: { username: 'felipepxavier' },
+      }
+    );
+
+    expect(usrData.email).toBe('Não cadastrado');
+    expect(usrData.location).toBe('Não cadastrado');
   });
 });
