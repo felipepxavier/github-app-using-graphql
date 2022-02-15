f2f44e4958e5930d614718b278c5550de36892dd
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2022-02-15 01:21:29 -0300

    test: should render link if existing

--- packages/app/src/components/Card/Card.test.tsx
+++ packages/app/src/components/Card/Card.test.tsx
@@ -1,25 +1,29 @@
 import { screen } from '@testing-library/react';
 import { renderWithTheme } from '../../utils/tests/helpers';
+import { MdKeyboardArrowLeft } from 'react-icons/md';
 import { FaGithub } from 'react-icons/fa';
 import { Card } from '.';
+import { Link } from 'react-router-dom';
 
 type makeSutProps = {
   title?: string;
   children?: JSX.Element;
   iconTitle?: JSX.Element;
+  linkTitle?: JSX.Element;
 };
 const makeSut = (
   {
     title = 'Pesquise um username',
     children = <span>children</span>,
     iconTitle,
+    linkTitle,
   }: makeSutProps = {
     title: 'Pesquise um username',
     children: <span>children</span>,
   }
 ) => {
   renderWithTheme(
-    <Card title={title} iconTitle={iconTitle}>
+    <Card title={title} iconTitle={iconTitle} linkTitle={linkTitle}>
       {children}
     </Card>
   );
@@ -44,5 +48,18 @@ describe('<Card />', () => {
     });
 
     expect(screen.getByTestId('icon')).toBeInTheDocument();
+  });
+
+  it('should render link if existing', () => {
+    makeSut({
+      iconTitle: <FaGithub data-testid="icon" />,
+      linkTitle: (
+        <Link to={'/'} data-testid="link">
+          <MdKeyboardArrowLeft />
+        </Link>
+      ),
+    });
+
+    expect(screen.getByTestId('link')).toBeInTheDocument();
   });
 });
