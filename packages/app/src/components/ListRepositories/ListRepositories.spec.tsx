c428c50f650ce17c55bc6ef360d93c3d841286a3
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2022-02-14 21:27:14 -0300

    test: should render more info button

--- packages/app/src/components/ListRepositories/ListRepositories.spec.tsx
+++ packages/app/src/components/ListRepositories/ListRepositories.spec.tsx
@@ -26,4 +26,25 @@ describe('<ListRepositories />', () => {
     expect(screen.getByText(list[0].language.name)).toBeInTheDocument();
     expect(screen.getByText(list[1].language.name)).toBeInTheDocument();
   });
+
+  it('should render more info button', () => {
+    const list = [
+      {
+        description: 'Apple Music web player',
+        language: { name: 'TypeScript', color: '#0076c6' },
+        name: 'apple-music-web-player',
+        visibility: 'public',
+      },
+      {
+        description:
+          'Plugin for artillery.io that signs HTTP requests using the AWS Signature V4 specification.',
+        language: { name: 'Não cadastrado', color: '#d2691e' },
+        name: 'artillery-plugin-aws-sigv4',
+        visibility: 'public',
+      },
+    ];
+    renderWithTheme(<ListRepositories listData={list} />);
+
+    expect(screen.getByTestId('more-info')).toBeInTheDocument();
+  });
 });
